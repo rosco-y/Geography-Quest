d3b28a79494eee2b1fbff90fa4bcb27014133254
Ross Ylitalo <ross.ylitalo@gmail.com>
2019-09-04 16:45:26 -0400

    coded in the chapter 1 version of the GameCamera and PlayerController scripts.

---
+++ GeographyQuest/Assets/Scripts/GameCamera.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+
+        #region PUBLIC VARS
+        public GameObject _trackObj;
+        public float _height;
+        public float _desiredDistance;
+        public float _heightDamp;
+        public float _rotDamp;
+        #endregion
+
+        // Update is called once per frame
+        void Update()
+        {
+                UpdateRotAndTrans();
+        }
+
+        void UpdateRotAndTrans()
+        {
+                  if (_trackObj)
+                {
+                        // target angle and height
+                        float desiredRotationAngle = _trackObj.transform.eulerAngles.y;
+                        float desiredHeight = _trackObj.transform.position.y + _height;
+
+                        // actual angle and height.
+                        float rotAngle = transform.eulerAngles.y;
+                        float height = transform.position.y;
+
+                        // LERP
+                        rotAngle = Mathf.LerpAngle(rotAngle, desiredRotationAngle, _rotDamp);
+                        height = Mathf.Lerp(height, desiredHeight, _heightDamp * Time.deltaTime);
+
+                        // move camera
+                        Quaternion curRotation = Quaternion.Euler(0f, rotAngle, 0f);
+                        Vector3 pos = _trackObj.transform.position;
+                        pos -= curRotation * Vector3.forward * _desiredDistance;
+                        pos.y = height;
+                        transform.position = pos;
+                }
+                else
+                {
+                        Debug.LogError("GameCamera:Error, _trackObj invalid.");
+                }
+        }
+}
---
+++ GeographyQuest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+        #region PUBLIC VARS
+        public Vector3 _moveDirection = Vector3.zero;
+        public float _rotateSpeed;
+        public float _moveSpeed;
+        public float _speedSmoothing = 10.0f;
+        #endregion
+
+        // Update is called once per frame
+        void Update()
+        {
+                UpdateMovement();
+        }
+
+        void UpdateMovement()
+        {
+                Vector3 cameraForward = Camera.main.transform.TransformDirection(Vector3.forward);
+
+                cameraForward.y = 0f;
+                cameraForward.Normalize();
+
+                Vector3 cameraRight = new Vector3(cameraForward.z, 0f, -cameraForward.x);
+
+                float vertical = Input.GetAxis("Vertical");
+                float horizontal = Input.GetAxis("Horizontal");
+
+                Vector3 targetDirection = horizontal * cameraRight + vertical * cameraForward;
+
+                if (targetDirection != Vector3.zero)
+                {
+                        _moveDirection = Vector3.RotateTowards(_moveDirection, targetDirection,
+                                _rotateSpeed * Mathf.Deg2Rad * Time.deltaTime, 1000);
+                        _moveDirection = _moveDirection.normalized;
+                }
+
+                // LERP
+                float curSmooth = _speedSmoothing * Time.deltaTime;
+                float targetSpeed = Mathf.Min(targetDirection.magnitude, 1f);
+                _moveSpeed = Mathf.Lerp(_moveSpeed, targetSpeed, curSmooth);
+
+                Vector3 displacement = _moveDirection * _moveSpeed * Time.deltaTime;
+
+                this.GetComponent<CharacterController>().Move(displacement);
+                transform.rotation = Quaternion.LookRotation(_moveDirection);
+        }
+}
